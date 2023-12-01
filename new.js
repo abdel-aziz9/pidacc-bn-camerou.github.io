5e192aeacf24596f393b0e81b8dde442b618dcf7
abdel-aziz9 <137040982+abdel-aziz9@users.noreply.github.com>
2023-12-01 08:20:53 +0100

    first commit
    premier hebergement du site pidacc cm

---
+++ new.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  var slides = $('.slide');
+  var currentIndex = 0;
+
+  function showSlide(index) {
+    slides.removeClass('active');
+    slides.eq(index).addClass('active');
+  }
+
+  function nextSlide() {
+    currentIndex++;
+    if (currentIndex >= slides.length) {
+      currentIndex = 0;
+    }
+    showSlide(currentIndex);
+  }
+
+  setInterval(nextSlide, 5000); // Changer d'image toutes les 5 secondes
+});
+
+// Utiliser jQuery pour ajouter un effet de déroulement
+$(document).ready(function() {
+  $('.zone').click(function() {
+    $(this).find('ul').slideToggle();
+  });
+});
