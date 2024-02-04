1fe0a4ccc5405edb30618b8baf8c58776ea10218
abdel-aziz9 <137040982+abdel-aziz9@users.noreply.github.com>
2024-02-04 12:10:19 +0100

    Add files via upload
    udate dimanche

--- info.js
+++ info.js
@@ -1,17 +1,13 @@
-// Sélectionner le slider
-const slider = document.querySelector('.slider');
 
-// Obtenir la largeur d'une image
-const imageWidth = slider.clientWidth / 3;
-
-// Défiler toutes les images dans le slider
-function slideImages() {
-  slider.style.transform = translateX(-${imageWidth}px);
-  setTimeout(() => {
-    slider.appendChild(slider.firstElementChild);
-    slider.style.transform = 'translateX(0)';
-  }, 5000); // Défilement toutes les 5 secondes
-}
-
-// Appeler la fonction de défilement des images
-slideImages();
+$(document).ready(function() {
+    $("#news-slider").owlCarousel({
+        items : 3,
+        itemsDesktop:[1199,3],
+        itemsDesktopSmall:[980,2],
+        itemsMobile : [600,1],
+        navigation:true,
+        navigationText:["",""],
+        pagination:true,
+        autoPlay:true
+    });
+});
